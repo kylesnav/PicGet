958ed3807f957127ce1de8d62e4cb525743fdff2
Kyle Snavely <kylesnav@gmail.com>
2012-09-14 13:44:39 -0700

    V0.4.3
    Cleaned up code, improved Tumblr functionality.

--- picget.py
+++ picget.py
@@ -4,41 +4,45 @@ import urllib, re, os
 def geturl():
 	url = raw_input('\n\nEnter a url (all pages of a Tumblr are saved): ')
 	rurl = raw_input('\nAlbum title (your choice): ')
+	if (url[-11:] == '.tumblr.com' or '.tumblr.com' in url) and (url[-14:-11] != 'www'):
+		turl = 'tumblr'
+	else:
+		turl = 'url'
 	if url[0:7] != 'http://':
 		url = 'http://' + url
 	print '\n' + url + ' is will be saved as: ' + rurl
 	print '\nFinding '+ url +', this may take a while. \n'
-	return url, rurl
+	return url, rurl, turl
 
 
 def gethtml(url):
-	#tumblr
-	if url[-11:] == '.tumblr.com':
-		name = url
+	rhtml = urllib.urlopen(url)
+	rhtml = rhtml.read()
+	html = ''
+	for i in rhtml:
+		html += i
+	return html
+
+def tumblrhtml(url):
+	name = url
+	if url[-1] != '/':
 		url = url + '/page/'
-		x = 0
-		fhtml = ''
-		while x >= 0:
-			x += 1
-			rhtml = urllib.urlopen(url + str(x))
-			rhtml = rhtml.read()
-			html = ''
-			for i in rhtml:
-				html += i
-			fhtml += html
-			if '"post-body' not in html:
-				break
-		html = fhtml
-		print str(x), 'pages of images being saved from:', name + '\n'
-	#not tumblr
 	else:
-		rhtml = urllib.urlopen(url)
+		url = url + 'page/'
+	x = 0
+	fhtml = ''
+	while x >= 0:
+		rhtml = urllib.urlopen(url + str(x + 1))
 		rhtml = rhtml.read()
 		html = ''
 		for i in rhtml:
 			html += i
+		fhtml += html
+		if '"post-body' not in html: break
+		else: x += 1
+	html = fhtml
+	print str(x), 'pages of images being saved from:', name + '\n'
 	return html
-
 
 def getlinks(html):
 	png = re.findall(r"src='.+\.png'", html) + re.findall(r'src=".+\.png"', html)
@@ -70,7 +74,7 @@ def finddir(rurl):
 	act = raw_input('\nPress enter, or type the name of the desired directory. ')
 	while act != '':
 		os.chdir(act)
-		print '\n\nYou are now currently in %s' % os.getcwd()
+		print '\nYou are now currently in %s' % os.getcwd()
 		print '\nThe current sub-directories are: %s' % os.listdir('.')
 		act = raw_input('\nPress enter, or type the name of the desired directory. ')
 	os.mkdir(rurl)
@@ -89,11 +93,12 @@ def getimgs(imgs):
 			os.remove(imgs[key])
 		else:
 			am +=1
-	print '\nSuccess, found ' + str(am) + ' image(s)!\n'
+	print 'Success, found ' + str(am) + ' image(s)!\n'
 
 def main():
-	url, rurl = geturl()
-	html = gethtml(url)
+	url, rurl, turl = geturl()
+	if turl == 'tumblr': html = tumblrhtml(url)
+	else: html = gethtml(url)
 	imgs = getlinks(html)
 	finddir(rurl)
 	getimgs(imgs)
